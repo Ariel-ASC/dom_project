b2b82d1e13f24096f2450a1dbaf219e1a346a4af
Ariel-ASC <astudilloariel40@gmail.com>
2024-07-29 11:37:59 -0400

    made changes

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 // Add your code to this file
+document.addEventListener('DOMContentLoaded', () => {
+    // Change the background color of the body
+    document.body.style.backgroundColor = "darkblue";
+});
